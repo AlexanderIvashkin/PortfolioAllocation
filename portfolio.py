86cdb20ba6de6fbc7c648ad5a8d58a7761198019
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-12-15 22:53:49 +0100

    WIP

--- portfolio.py
+++ portfolio.py
@@ -14,3 +14,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+def buy_an_asset(assetsToBuy, moneyLeft, assetID = 0):
+    currAsset = assetsToBuy[assetID]
+    if len(assetsToBuy) == 0:
+        return moneyLeft//currAsset[1]
+
+    nextAsset = assetsToBuy[-1]
+
+    for currAssCount in range(0, moneyLeft//currAsset[1]):
+        currMoneyLeft = moneyLeft - currAsset[1] * currAssCount
+        nextAssCount = buy_an_asset(assetsToBuy, currMoneyLeft)
+        print("Buying ", currAssCount, " of ", currAsset[0], " at ", currAsset[1])
+        print("Buying ", nextAssCount, " of ", nextAsset[0], " at ", nextAsset[1])
+        print("Leftover money: ", currMoneyLeft - nextAsset[1] * nextAssCount)
+
+
+buy_an_asset([("SCHA", 2, 0.02), ("S&P", 3, 0.01)], 10)
